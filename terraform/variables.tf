2075e067c57c8cba35c230cfae79bd3891bbc24c
Emmanuel Bergerat <45499891+gopher194@users.noreply.github.com>
2023-04-25 12:20:20 -0400

    variables formatted

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
-variable tenant_id       {}
-variable subscription_id {}
-variable client_id       {}
-variable client_secret   {}
+variable "tenant_id" {}
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
